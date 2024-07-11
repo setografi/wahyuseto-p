7168a17b19a70bbf77535e7e1529e81d07a8da5b
setografi <hitonaashbell@gmail.com>
2024-07-11 13:37:24 +0700

    Update: Responsive

--- src/components/NavbarEvent.jsx
+++ src/components/NavbarEvent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavbarEvent({ onSectionChange }) {
   return (
     <>
-      <nav className="max-w-3xl w-full mx-auto grid grid-cols-2 gap-4 items-start mt-5">
+      <nav className="max-w-3xl w-full items-center justify-start mx-auto mt-5">
         <ul className="flex justify-between bg-zinc-800 gap-x-2 p-2 rounded-xl">
           <button
             className="w-full bg-transparent text-whiteText text-base font-lexendDeca font-medium rounded-lg p-2 transition-all duration-300 cursor-pointer hover:transform-gpu hover:bg-whiteText hover:text-zinc-800"
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,6 +22,13 @@ export default {
       },
       animation: {
         "spin-slow": "spin 3s linear infinite",
+        rotate: "rotate 2s linear infinite",
+      },
+      keyframes: {
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
     },
   },
