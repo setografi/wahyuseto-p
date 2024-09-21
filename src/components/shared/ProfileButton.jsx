5320164960551841ca694d261ec7344061af59b1
setografi <hitonaashbell@gmail.com>
2024-09-21 12:59:20 +0700

    update: Project Grid

--- src/components/shared/ProfileButton.jsx
+++ src/components/shared/ProfileButton.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 export default function ProfileButton() {
+  const handleEmailClick = () => {
+    window.location.href = 'mailto:wahyusetotkrx18@gmail';
+  };
+
   return (
-    <div className="py-2">
+    <div className="py-6">
       <a
         href="mailto:wahyusetotkrx18@gmail"
         target="_blank"
-        className="flex-no-shrink rounded-full font-offBit text-sm tracking-widest text-whiteText transition duration-300 ease-in hover:text-magentaC"
+        className="rounded-lg bg-magentaC px-4 py-2 font-offBit text-sm tracking-widest text-whiteText transition duration-300 ease-in hover:bg-grayNetral hover:text-gray-400"
       >
         Email Me
       </a>
