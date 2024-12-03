883f7928eed78ea1750efe3055fe50d4414c6f6c
setografi <hitonaashbell@gmail.com>
2024-12-03 21:32:42 +0700

    update: Web Content 3

---
+++ src/constants/skill.ts
@@ -0,0 +1,129 @@
+export interface Skill {
+  name: string;
+  image?: string;
+  bgColor?: string;
+  textColor?: string;
+}
+
+export const skills: Skill[] = [
+  {
+    name: 'HTML',
+    image: './icon/html5.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'CSS',
+    image: './icon/css3.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Javascript',
+    image: './icon/javascript.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Typescript',
+    image: './icon/typescript.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'React JS',
+    image: './icon/react.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Tailwind CSS',
+    image: './icon/tailwindcss.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Next JS',
+    image: './icon/nextdotjs.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Astro JS',
+    image: './icon/astro.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Alpine JS',
+    image: './icon/alpinedotjs.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'React Native',
+    image: './icon/react.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Python',
+    image: './icon/python.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Flask',
+    image: './icon/flask.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'C#',
+    image: './icon/csharp.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Elixir',
+    image: './icon/elixir.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Phoenix Framework',
+    image: './icon/phoenixframework.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Adobe Photoshop',
+    image: './icon/adobephotoshop.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Adobe Lightroom',
+    image: './icon/adobelightroom.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'CorelDraw',
+    image: './icon/coreldraw.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Canva',
+    image: './icon/canva.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+  {
+    name: 'Figma',
+    image: './icon/figma.svg',
+    bgColor: 'bg-neutral-100',
+    textColor: 'text-neutral-900',
+  },
+];
